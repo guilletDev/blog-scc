318c255228823ad4ae2693ac647f9ad977c503d9
guilletDev <guilletdev@gmail.com>
2023-02-23 16:27:50 -0300

    modifique el logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,9 +5,9 @@ const siteMetadata = {
   description: 'Compartimos lo último en tecnología.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.svg',
+  siteUrl: 'https://blog-scc.vercel.app',
+  siteRepo: '',
+  siteLogo: '/static/images/scc.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
