dc137123b7d3a7c3c6c13f449c7db2b870df38c0
guilletDev <guilletdev@gmail.com>
2023-03-17 16:03:24 -0300

    cambio en el socialicon instagram del footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="instagram" href={siteMetadata.instagram} size="8" />
+          <SocialIcon kind="instagram" href={siteMetadata.instagram} size="6" />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
